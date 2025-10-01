6ab27ad7d7769e9794a19a8b3add57cee66dec36
Gennady Matveev <g.matveev@okko.tv>
2025-10-01 18:09:15 +0400

    cors

--- src/interface/api/app.py
+++ src/interface/api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.interface.api.frontend import frontend
 from src.interface.api.v1 import router
@@ -6,6 +7,13 @@ from src.interface.api.v1 import router
 
 def create_app() -> FastAPI:
     app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Разрешить все домены
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(router, prefix="/api/v1")
     app.include_router(frontend)
     return app

421931f07c338d8314d1133bb7349af93382194b
Gennady Matveev <g.matveev@okko.tv>
2025-10-01 17:39:55 +0400

    nginx

--- store.py
+++ store.py
@@ -31,7 +31,7 @@ def save_kp_collections():
 def api():
     uvicorn.run(
         app=create_app(),
-        # host="0.0.0.0",
+        host="0.0.0.0",
         port=8000,
         workers=1,
         loop="uvloop",
